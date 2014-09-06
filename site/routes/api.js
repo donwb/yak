df2f823f9fd967629d76ada87f96d9f75007cf4f
Don Browning <donwb@Dons-MacBook-Pro.local>
2014-09-06 11:40:24 -0400

    added sorting

--- site/routes/api.js
+++ site/routes/api.js
@@ -10,13 +10,13 @@ console.log(dbstring);
 
 router.get('/yaksForArea/:area', function(req, res) {
 	var area = parseInt(req.params.area);
-	console.log(area);
+	//console.log(area);
 	
 	if(isNaN(area)) {
 		var msg = req.params.area + ' is not a valid region';
 		res.send({"Status": msg})
 	} else {
-		runQuery("region", area, function(returnValue) {
+		runQuery("region", area, 200, function(returnValue) {
 			res.send(returnValue);
 		});
 	}
@@ -24,24 +24,24 @@ router.get('/yaksForArea/:area', function(req, res) {
 
 router.get('/yaksForUser/:userId', function(req, res){
 	var user = parseInt(req.params.userId);
-	console.log(user);
+	//console.log(user);
 
 	if(isNaN(user)) {
 		var msg = req.params.user + ' is not a valid user';
 		res.send({"Status": msg});
 	} else {
-		runQuery("userid", user, function(returnValue) {
+		runQuery("userid", user, 200, function(returnValue) {
 			res.send(returnValue);
 		})
 	}
 
 });
 
-function runQuery(field, value, callback) {
+function runQuery(field, value, limit, callback) {
 	var db = mongo.db(dbstring, {native_parser: true});
 	db.bind('yaks');
 	if(!(isNaN(parseInt(value)))) {
-		console.log('inting this bitch');
+		//console.log('inting this bitch');
 		value = parseInt(value);
 	}
 
@@ -50,8 +50,8 @@ function runQuery(field, value, callback) {
 	var query = {};
 	query[key] = value
 
-	db.yaks.find(query).toArray(function(err, items){
-		console.log(items);
+	db.yaks.find(query).limit(limit).sort({"created":-1}).toArray(function(err, items){
+		//console.log(items);
 		if(items.length === 0) {
 			callback({"Status" : "User not found"});
 		} else {
