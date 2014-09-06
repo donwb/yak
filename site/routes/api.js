06332666cb14555680a1e4641973e0bb452d7bd2
Don Browning <donwb@Dons-MacBook-Pro.local>
2014-09-05 20:34:25 -0400

    added new api

--- site/routes/api.js
+++ site/routes/api.js
@@ -9,25 +9,60 @@ var dbstring = config.dbstring;
 console.log(dbstring);
 
 router.get('/yaksForArea/:area', function(req, res) {
-	var area = req.params.area;
+	var area = parseInt(req.params.area);
 	console.log(area);
+	
+	if(isNaN(area)) {
+		var msg = req.params.area + ' is not a valid region';
+		res.send({"Status": msg})
+	} else {
+		runQuery("region", area, function(returnValue) {
+			res.send(returnValue);
+		});
+	}
+});
 
-  	
+router.get('/yaksForUser/:userId', function(req, res){
+	var user = parseInt(req.params.userId);
+	console.log(user);
 
-  	var db = mongo.db(dbstring, {native_parser: true});
+	if(isNaN(user)) {
+		var msg = req.params.user + ' is not a valid user';
+		res.send({"Status": msg});
+	} else {
+		runQuery("userid", user, function(returnValue) {
+			res.send(returnValue);
+		})
+	}
+
+});
+
+function runQuery(field, value, callback) {
+	var db = mongo.db(dbstring, {native_parser: true});
 	db.bind('yaks');
+	if(!(isNaN(parseInt(value)))) {
+		console.log('inting this bitch');
+		value = parseInt(value);
+	}
 
-	db.yaks.find({"region": 98}).toArray(function(err, items){
+	// For real the only way this works?!?!
+	var key = field;
+	var query = {};
+	query[key] = value
+
+	db.yaks.find(query).toArray(function(err, items){
+		console.log(items);
 		if(items.length === 0) {
-			res.send({"Status" : "Region not found"});
+			callback({"Status" : "User not found"});
 		} else {
-			res.send({region: items});
+			callback(items);
 		}
 
 		db.close();
 	});
-
-
-});
+}
 
 module.exports = router;
+
+
+
