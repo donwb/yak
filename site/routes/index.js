8260455f7826f2794e117f3e081e8ae02e1c8e34
Don Browning <donwb@Dons-MacBook-Pro.local>
2014-09-06 11:53:07 -0400

    added loader.io token

--- site/routes/index.js
+++ site/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/loaderio-8369bf1bdbb669777ff0a1972ceabde0', function(req, res) {
+	res.send('loaderio-8369bf1bdbb669777ff0a1972ceabde0');
+});
+
 module.exports = router;
